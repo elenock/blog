cdf6a3f9a5bd2bde3e743362cd0d4116d755b5cb
elenock <zaxarenko.elena@gmail.com>
2019-09-27 17:03:30 +0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.6.3'
+gem 'active_model_serializers'
+gem 'bcrypt'
+gem 'bootsnap', '>= 1.1.0', require: false
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'pg'
+gem 'pry-rails'
+gem 'puma', '~> 3.11'
+gem 'rails', '~> 5.2.3'
+gem 'uglifier'
+#gem 'sidekiq'
+#gem 'faker'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
+  gem 'with_advisory_lock'
+end
+
+group :test do
+  gem 'database_cleaner'
+end
+
+group :development do
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'rubocop'
+  gem 'rubocop-config-umbrellio'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
---
+++ spec/spec_helper.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') unless Rails.env.test?
+
+require 'rspec/rails'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each(&method(:require))
+
+RSpec.configure do |config|
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  config.include FactoryBot::Syntax::Methods
+  config.include SpecSupport::JSONHelpers
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  # config.example_status_persistence_file_path = "spec/examples.txt"
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  # config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+end
