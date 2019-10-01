dcdfdc2484ebd465e7855cc003cec4f3f03785b6
elenock <zaxarenko.elena@gmail.com>
2019-10-01 15:34:04 +0300

    fix rubocop offenses (2)

--- spec/support/json_helpers.rb
+++ spec/support/json_helpers.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-module SpecSupport::JSONHelpers
-  def json_response
-    JSON.parse(response.body, symbolize_names: true)
+# rubocop:disable Style/ClassAndModuleChildren
+module SpecSupport
+  module JSONHelpers
+    def json_response
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
+# rubocop:enable Style/ClassAndModuleChildren
